4a37290116beebbfb0de3df34fde966eb13266b6
THOMAS LAVELLE <thomas.lavelle@jpmchase.com>
2016-01-13 12:12:18 -0500

    Initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {Ang2App} from './app/ang2';
+import {ROUTER_PROVIDERS} from 'angular2/router';
 
 
-bootstrap(Ang2App);
+bootstrap(Ang2App, [
+  ROUTER_PROVIDERS
+]);
---
+++ src/app/components/my-new-component/my-new-component.spec.ts
@@ -0,0 +1,27 @@
+import {
+  it,
+  iit,
+  describe,
+  ddescribe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+import {provide} from 'angular2/core';
+import {MyNewComponent} from './my-new-component';
+
+
+describe('MyNewComponent Component', () => {
+
+  beforeEachProviders(() => []);
+
+
+  it('should ...', injectAsync([TestComponentBuilder], (tcb:TestComponentBuilder) => {
+    return tcb.createAsync(MyNewComponent).then((fixture) => {
+      fixture.detectChanges();
+    });
+  }));
+
+});
